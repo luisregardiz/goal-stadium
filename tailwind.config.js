4efdb8bd6fd6152cb29d3012f155fbc0b0b2a5dc
Luis Garcia Regardiz <gadeeks@gmail.com>
2021-12-15 01:58:43 -0400

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {},
+        fontFamily: {
+            sans: ["soccer_league", "sans-serif"],
+        },
+    },
+    plugins: [],
+};
