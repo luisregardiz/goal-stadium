a4ab24791896c478aee516dca03717140ef485b7
Luis Garcia Regardiz <gadeeks@gmail.com>
2021-12-15 16:38:05 -0400

    Adding the shop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: ["./dist/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "goal-blue-100": "#123c5e",
+                "goal-blue-200": "#124887",
+                "goal-purple-100": "#372c54",
+                "goal-gray-100": "#bbb0df",
+            },
+        },
         fontFamily: {
             sans: ["soccer_league", "sans-serif"],
         },
